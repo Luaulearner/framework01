3b22e52ab94a1f1b9b013c11fce584408bf88822
Luaulearner <ppggyar@gmail.com>
2025-01-01 19:32:18 +0800

    update projectwork_0.lua

--- projectwork_0.lua
+++ projectwork_0.lua
@@ -7,13 +7,27 @@ if not game:IsLoaded() then
     task.wait()
     until game:IsLoaded()
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library//main/Library", true))()
+task.wait(0.1)
+local blackscreen = Instance.new("ScreenGui")
+blackscreen.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+local theframe = Instance.new("Frame")
+theframe.Parent = blackscreen
+theframe.Size = UDim2.new(999, 999, 999)
+local playthemesong = Instance.new("Sound")
+playthemesong.SoundId = "rbxassetid://98107180135263"
+playthemesong.Volume = 10
+playthemesong.Parent = game.SoundService
+playthemesong:Play()
 
+playthemesong.Ended:Connect(function()
+  
 
-local window = library:AddWindow("", {
-	main_color = Color3.fromRGB(41, 74, 122), -- Color
+local window = library:AddWindow("Black Mesa Research Facility Incident +", {
+	main_color = Color3.fromRGB(0, 1, 0), -- Color
 	min_size = Vector2.new(250, 346), -- Size of the gui
-	can_resize = false, -- true or false
+	can_resize = true, -- true or false
 })
+
 
 
 end
