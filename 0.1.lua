24d688eaa455f42366ff822d70c41dbe536b0c10
Luaulearner <ppggyar@gmail.com>
2025-01-12 14:04:08 +0800

    update 0.1.lua

--- 0.1.lua
+++ 0.1.lua
@@ -1,1 +1,8 @@
 --/0/0/
+local p = game.Players.LocalPlayer.Character
+getgenv().ver = function()
+  local vers = {
+    p.Humanoid.WalkSpeed = 0
+    game.Workspace.Camera.FieldOfView = 120
+    print("01")
+  }
