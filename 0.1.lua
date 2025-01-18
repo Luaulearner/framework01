3c1a12eb7497b1508f3fa68883e58de5d293cb19
Luaulearner <ppggyar@gmail.com>
2025-01-18 17:15:45 +0800

    update 0.1.lua

--- 0.1.lua
+++ 0.1.lua
@@ -1,5 +1,6 @@
-local medf = "https://www.mediafire.com/file/5nwg94panmux6gw/PULSE+(Extremly+Slowed).mp3/file"
-local ap = "player000/r.mp3"
+getgenv().ss = function()
+local medf = "https://drive.google.com/uc?export=download&id=1-I2R-ad97Wu959UD-3am9tRBmKeetV8t"
+local ap = "player000/q.mp3"
 
 if not isfile(ap) then
   warn("Not Downloaded.")
@@ -16,4 +17,5 @@ if isfile(ap) then
   super.Volume = math.huge
   super.Looped = true
   super:Play()
+end
 end
