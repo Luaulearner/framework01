16d1fd139a6a97df18203c21490f21109d9b2b02
Luaulearner <ppggyar@gmail.com>
2025-01-26 22:39:52 +0800

    update 0.1.lua

--- 0.1.lua
+++ 0.1.lua
@@ -3,6 +3,11 @@ local e = workspace.CurrentCamera
 local a = Instance.new("Part")
 local bee = bool
 if bee == true then
+  for _, v in pairs(game:GetDescendants()) do
+    if v:IsA("Particle") or v:IsA("Mesh") or v:IsA("SpecialMesh") then
+      v:Destroy()
+    end
+  end
 a.Parent = workspace
 a.Position = Vector3.new(9999999, 999999999, 99999999)
 e.CameraSubject = a
