79c81f0aebc1c0067d1b4a3a9d5637a1a622e4a6
Luaulearner <ppggyar@gmail.com>
2025-01-29 11:17:49 +0800

    update 0.1.lua

--- 0.1.lua
+++ 0.1.lua
@@ -1,19 +1,3 @@
-getgenv().delpl = function(bool)
-local e = workspace.CurrentCamera
-local a = Instance.new("Part")
-local bee = bool
-if bee == true then
-  for _, v in pairs(game:GetDescendants()) do
-    if v:IsA("Particle") or v:IsA("Mesh") or v:IsA("SpecialMesh") then
-      v:Destroy()
-    end
-  end
-a.Parent = workspace
-a.Position = Vector3.new(9999999, 999999999, 99999999)
-e.CameraSubject = a
-e.CameraType = Enum.CameraType.Custom
-else
-  e.CameraSubject = game.Players.LocalPlayer.Character.Humanoid
-  e.CameraType = Enum.CameraType.Custom
-end
+for i, v in pairs(workspace.Items:GetChildren()) do
+  print(i)
 end
