c0f83ec993ffb8a7fadedd1ab6f2da847a743528
Luaulearner <ppggyar@gmail.com>
2025-02-02 13:48:11 +0800

    update 0.1.lua

--- 0.1.lua
+++ 0.1.lua
@@ -1,3 +1,16 @@
-for i, v in pairs(workspace.Items:GetChildren()) do
-  print(i)
+local p = game:GetService("Players")
+local u = p.LocalPlayer
+local targets = game:GetService("Teams"):FindFirstChild("Raiders")
+local targets2 = game:GetService("Teams"):FindFirstChild("Outsiders")
+for _, player in pairs(p:GetPlayers()) do
+  if player ~= u and player.Team == targets2 or player.Team == targets then
+    for _, v in pairs(workspace.GetDescendants()) do
+      if v:IsA("Player") and v:FindFirstChild("HumanoidRootPart") then
+        v.Size = Vector3.new(16, 15, 15)
+        v.Collision = false
+        v.Transparency = 0.9
+        v.BrickColor = Color3.fromRGB(255, 0, 15)
+      end
+    end
+  end
 end
